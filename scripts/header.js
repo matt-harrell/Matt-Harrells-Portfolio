6de59d9838eccd4f5b29b4a70d726baca5b1f881
Matt Harrell <matthew.629@hotmail.com>
2021-07-12 16:30:37 -0400

    made template literal

---
+++ scripts/header.js
@@ -0,0 +1,18 @@
+ const header = `
+
+      <div class="row">
+          <div class="col-sm">
+
+            <a href="index.html"><img src="images/logo.svg" class="img-fluid logo mt-3" alt="matt harrell with h made with green wire"></a>
+          </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm">
+          <p class="mt-2 tagLine">Web Design and Development, UI/UX, Interactive Design</p>
+        </div>
+      </div>    
+
+ `;
+
+ $(header).appendTo( ".container.header" );
---
+++ scripts/nav.js
@@ -0,0 +1,32 @@
+const navBar = `
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark m-0 p-0 d-flex justify-content-end">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+          <ul class="navbar-nav w-100 d-flex justify-content-between">
+
+            <li class="nav-item flex-fill p-0">
+              <a class="nav-link btn-dark text-center" href="index.html">Portfolio</a>
+            </li>
+
+            <li class="nav-item flex-fill">
+              <a class="nav-link btn-dark text-center" href="resume.html">Resume</a>
+            </li>
+
+            <li class="nav-item flex-fill">
+              <a class="nav-link text-center btn-dark" href="contact.html">Contact</a>
+            </li>
+
+          </ul>
+
+        </div>
+        
+    </nav>
+
+`;
+
+$(".container.header").after(navBar);
